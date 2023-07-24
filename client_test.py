36b38ce6628d9400daa1cae62bf004c237042e38
Madeline <madelinebird@gmail.com>
2023-07-24 11:20:39 -0400

    Step 1 complete

--- client_test.py
+++ client_test.py
@@ -7,17 +7,28 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
 
+    # Expected data points
+    expected_data_point_1 = ('ABC', 120.48, 121.2, (120.48 + 121.2) / 2)
+    expected_data_point_2 = ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)
+
+    # Test assertions
+    self.assertEqual(getDataPoint(quotes[0]), expected_data_point_1)
+    self.assertEqual(getDataPoint(quotes[1]), expected_data_point_2)
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
 
+    # Expected data points
+    expected_data_point_1 = ('ABC', 120.48, 119.2, (120.48 + 119.2) / 2)
+    expected_data_point_2 = ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)
 
-  """ ------------ Add more unit tests ------------ """
+    # Test assertions
+    self.assertEqual(getDataPoint(quotes[0]), expected_data_point_1)
+    self.assertEqual(getDataPoint(quotes[1]), expected_data_point_2)
+
 
 
 
